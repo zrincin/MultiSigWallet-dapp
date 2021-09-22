6f6cc7c42244820ffc4033a3e96eb1a36064f24e
zrincin <z.zrakoplovac@gmail.com>
2021-09-22 06:09:06 +0200

    change first owner  account from [1] to [0]

--- src/migrations/2_MultiSigWallet_migration.js
+++ src/migrations/2_MultiSigWallet_migration.js
@@ -1,5 +1,5 @@
 const MSW = artifacts.require('MultiSigWallet.sol');
 
 module.exports = (deployer, network, accounts) => {
-    deployer.deploy(MSW, [accounts[1], accounts[2], accounts[3]], 3, {from: accounts[0]})
+    deployer.deploy(MSW, [accounts[0], accounts[1], accounts[2]], 3, {from: accounts[0]})
 };
