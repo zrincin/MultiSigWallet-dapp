551d90d7942fb1d5db5574cbe61699e33be869fa
zrincin <z.zrakoplovac@gmail.com>
2021-11-08 19:30:56 +0100

    update deprecated method

--- src/web3.js
+++ src/web3.js
@@ -1,24 +1,21 @@
-import Web3 from 'web3';
+import Web3 from "web3";
 
 let web3;
 
 // Modern DAPP browsers
 if (window.ethereum) {
-    web3 = new Web3(window.ethereum);
-    window.ethereum.enable();
-} 
+  window.ethereum.request({ method: "eth_requestAccounts" }); // instead of "window.ethereum.enable()" - deprecated
+  web3 = new Web3(window.ethereum);
+}
 
 // Legacy DAPP browsers
 else if (window.web3) {
-    web3 = new Web3(window.web3.currentProvider);
-    }
-
-else {
-    window.alert(`
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  window.alert(`
     Metamask not detected. 
     You need to install it in order to be able to use the DAPP! 
-    (https://metamask.io/index.html)` )
+    (https://metamask.io/index.html)`);
 }
-
 
 export default web3;
