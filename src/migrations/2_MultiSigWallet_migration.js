0807ab75d49b54a72d57e1775edfe0d9b5e789e6
zrincin <z.zrakoplovac@gmail.com>
2021-12-01 23:28:14 +0100

    redeploy new instance of smart contract  to Rinkeby via Moralis nodes

--- src/migrations/2_MultiSigWallet_migration.js
+++ src/migrations/2_MultiSigWallet_migration.js
@@ -1,5 +1,11 @@
-const MSW = artifacts.require('MultiSigWallet.sol');
+const MSW = artifacts.require("MultiSigWallet.sol");
 
-module.exports = (deployer, network, accounts) => {
-    deployer.deploy(MSW, [accounts[0], accounts[1], accounts[2]], 3, {from: accounts[0]})
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(MSW, [accounts[0], accounts[1], accounts[2]], 3);
+  const wallet = await MSW.deployed();
+  web3.eth.sendTransaction({
+    from: accounts[1],
+    to: wallet.address,
+    value: 1000000000000000000, // 1 ether
+  });
 };
