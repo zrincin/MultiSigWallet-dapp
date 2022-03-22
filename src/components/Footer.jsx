75cedff9032dc884df7ca6fcd37eb74710c4068d
zrincin <z.zrakoplovac@gmail.com>
2022-03-22 03:23:54 +0100

    add new component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#ECF0F1",
+        position: "fixed",
+        width: "100%",
+        left: 0,
+        bottom: 0,
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        &copy; ZrinCin, {new Date().getFullYear()}
+        {"."}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/TransferList.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import web3 from "../web3";
+import "./TransferList.css";
+
+const TransferList = ({ transfers, approveTransfer }) => {
+  return (
+    <div style={{ marginBottom: 50 }}>
+      <h4>Transfers:</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Amount [ETH] &nbsp;&nbsp;</th>
+            <th>Beneficiary</th>
+            <th>Approvals &nbsp;&nbsp; </th>
+            <th>Sent </th>
+          </tr>
+        </thead>
+        <tbody>
+          {transfers.map((transfer) => (
+            <tr key={transfer.ID}>
+              <td>{transfer.ID} &nbsp;&nbsp;&nbsp;</td>
+              <td>
+                {web3.utils.fromWei(transfer.amount, "ether")}
+                &nbsp;&nbsp;&nbsp;
+              </td>
+              <td>{transfer.to} &nbsp;&nbsp;&nbsp;</td>
+              <td style={{ textAlign: "center" }}>
+                {transfer.approvals} &nbsp;&nbsp;&nbsp;
+              </td>
+              <td className={`${transfer.isSent ? "sent" : ""}`}>
+                {transfer.isSent ? "yes" : "no"}
+              </td>
+              <button
+                className={`${!transfer.isSent ? "approve-btn" : "hide-btn"}`}
+                style={{ marginLeft: 20 }}
+                onClick={() => approveTransfer(transfer.ID)}
+              >
+                Approve
+              </button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TransferList;
